8c404fd3b88cc44f617419ba134acbfd8883295b
Bas Zalmstra <zalmstra.bas@gmail.com>
2020-10-17 21:57:58 +0200

    feature: command line arguments

--- crates/my-little-robots/src/bin/mlr.rs
+++ crates/my-little-robots/src/bin/mlr.rs
@@ -2,98 +2,143 @@ use mlr::application;
 use mlr::runner::Runner;
 use mlr::GameState;
 use mlr::Player;
-use mlr::{random_direction, World};
-use mlr_api::{Coord, PlayerAction, PlayerId, PlayerInput, PlayerOutput, RunnerError, Unit};
+use mlr::{World};
+use mlr_api::{Coord, PlayerId};
+use structopt::StructOpt;
+use std::ffi::{OsString, OsStr};
+use anyhow::Context;
+use std::path::PathBuf;
+use anyhow::{bail, anyhow};
+use itertools::Itertools;
 use serde_json::json;
 
-fn player_run(input: PlayerInput) -> Result<PlayerOutput, RunnerError> {
-    let mut rng = rand::thread_rng();
+#[derive(StructOpt)]
+#[structopt(name = "my-little-robots CLI", author, setting = clap::AppSettings::DeriveDisplayOrder)]
+enum MyLittleRobots {
+    /// Command for running a local match
+    Run(Run),
+}
 
-    // Get all units
-    let (my_units, _other_units): (Vec<&Unit>, Vec<&Unit>) = input
-        .world
-        .units
-        .iter()
-        .partition(|u| u.player == input.player_id);
-
-    // Move all units
-    let mut actions = Vec::new();
-    for unit in my_units {
-        actions.push(PlayerAction::Move(unit.id, random_direction(&mut rng)));
-    }
-
-    Ok(PlayerOutput {
-        actions,
-        memory: input.memory,
-    })
+#[derive(StructOpt)]
+#[structopt(setting = clap::AppSettings::DeriveDisplayOrder)]
+struct Run {
+    /// The runners that should be placed in the match.
+    ///
+    /// A runner is specified in one of the following ways:
+    /// 1. `command:$PATH` or `localrunner:$PATH`. The path to a binary file.
+    #[structopt(parse(from_os_str), required = true, min_values = 2, verbatim_doc_comment)]
+    runners: Vec<OsString>,
 }
 
 fn main() {
-    env_logger::init();
+    if let Err(err) = try_main() {
+        eprintln!("ERROR: {}", err);
+        err.chain().skip(1).for_each(|cause| eprintln!("because: {}", cause));
+        std::process::exit(1)
+    }
+}
 
-    let example_location = std::env::current_exe()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .join(format!("example-player{}", std::env::consts::EXE_SUFFIX));
-    println!(
-        "Assuming that the example-player application is located at: {}",
-        example_location.display()
-    );
+fn try_main() -> anyhow::Result<()>
+{
+    env_logger::try_init()?;
 
-    let mut game_state = GameState {
-        players: vec![
-            Player {
-                id: PlayerId(0),
-                runner: Box::new(Runner::new_cmd(example_location, vec!["drol"])),
-                memory: json!({}),
-            },
-            Player {
-                id: PlayerId(1),
-                runner: Box::new(player_run),
-                memory: json!({}),
-            },
-            Player {
-                id: PlayerId(2),
-                runner: Box::new(player_run),
-                memory: json!({}),
-            },
-            Player {
-                id: PlayerId(3),
-                runner: Box::new(player_run),
-                memory: json!({}),
-            },
-        ],
-        world: World::default(),
-        turn: 0,
-    };
+    let opt: MyLittleRobots = MyLittleRobots::from_args();
 
-    // Spawn a unit for every player
-    for (i, player) in game_state.players.iter().enumerate() {
-        game_state
-            .world
-            .spawn_unit(player.id, Coord::new(10 + i as isize * 10, 10));
+    match opt {
+        MyLittleRobots::Run(run_opt) => {
+            let players = run_opt.runners.iter().enumerate().map(|(i, r)| -> anyhow::Result<Player> {
+                let runner = RunnerDesc::parse(r)?;
+                Ok(Player {
+                    id: PlayerId(i),
+                    runner: Box::new(runner.into_runner()?),
+                    memory: json!({}),
+                })
+            }).collect::<Result<Vec<_>, _>>()?;
+
+            let mut game_state = GameState {
+                players,
+                world: World::default(),
+                turn: 0,
+            };
+
+            // Spawn a unit for every player
+            for (i, player) in game_state.players.iter().enumerate() {
+                game_state
+                    .world
+                    .spawn_unit(player.id, Coord::new(10 + i as isize * 10, 10));
+            }
+
+            // Create the world
+            let (sender, receiver) = async_watch::channel(game_state.world.clone());
+
+            std::thread::spawn(|| {
+                async_std::task::block_on(async move {
+                    // Run the turn in a loop
+                    loop {
+                        game_state = game_state.turn().await;
+                        if sender.send(game_state.world.clone()).is_err() {
+                            break; // Sender closed
+                        }
+                        if game_state.world.units_on_exits().next().is_some() {
+                            break;
+                        }
+                        async_std::task::sleep(std::time::Duration::from_millis(10)).await;
+                    }
+                });
+            });
+
+            // Render our world
+            application::run(receiver).expect("failed to render");
+        }
     }
 
-    // Create the world
-    let (sender, receiver) = async_watch::channel(game_state.world.clone());
+    Ok(())
+}
 
-    std::thread::spawn(|| {
-        async_std::task::block_on(async move {
-            // Run the turn in a loop
-            loop {
-                game_state = game_state.turn().await;
-                if sender.send(game_state.world.clone()).is_err() {
-                    break; // Sender closed
-                }
-                if game_state.world.units_on_exits().next().is_some() {
-                    break;
-                }
-                async_std::task::sleep(std::time::Duration::from_millis(10)).await;
+enum RunnerDesc {
+    Command {
+        command: String,
+        args: Vec<String>
+    }
+}
+
+impl RunnerDesc {
+    pub fn parse(s: &OsStr) -> anyhow::Result<Self> {
+        let s = match s.to_str() {
+            Some(s) => s,
+            None => return Self::from_path(PathBuf::from(s)),
+        };
+
+        let parse_command = |s| -> anyhow::Result<_> {
+            let mut args = shell_words::split(s).context("couldn't parse as shell arguments")?.into_iter();
+            let command = args.next().ok_or_else(|| {
+                anyhow!("you must have at least one shell 'word' in the command string")
+            })?;
+            Ok((command, args.collect_vec()))
+        };
+
+        if let Some((typ, content)) = s.splitn(2, ':').collect_tuple() {
+            match typ {
+                "file" | "local" => Self::from_path(PathBuf::from(content)),
+                "command" => {
+                    let (command, args) = parse_command(content)?;
+                    Ok(Self::Command { command, args })
+                },
+                _ => bail!("unknown runner type {:?}", typ),
             }
-        });
-    });
+        } else {
+            Self::from_path(PathBuf::from(s))
+        }
+    }
 
-    // Render our world
-    application::run(receiver).expect("Error while rendering");
+    fn from_path(_source: PathBuf) -> anyhow::Result<Self> {
+        bail!("we do not yet support running from path")
+    }
+
+    /// Construct a runner from this description
+    pub fn into_runner(self) -> anyhow::Result<Runner> {
+        match self {
+            RunnerDesc::Command { command, args } => Ok(Runner::new_cmd(command, args))
+        }
+    }
 }
