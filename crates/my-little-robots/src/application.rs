f4be2fac9ab56de38ac02e0bb77ffb1e70015ead
Bas Zalmstra <zalmstra.bas@gmail.com>
2020-10-19 22:23:56 +0200

    misc: nicer walls

--- crates/my-little-robots/src/application.rs
+++ crates/my-little-robots/src/application.rs
@@ -24,7 +24,7 @@ impl GameState for ApplicationState {
 fn glyph_for(coord: Coord, map: &Map) -> (impl Into<RGBA>, FontCharType) {
     let tile_type = map[coord];
     match tile_type {
-        TileType::Wall => (WHITE, to_cp437('#')),
+        TileType::Wall => (WHITE, wall_glyph(map, coord.x, coord.y)),
         TileType::Floor => (GRAY, to_cp437('.')),
         TileType::Exit => (CYAN, to_cp437('>')),
     }
@@ -47,6 +47,50 @@ fn unit_glyph(unit: &Unit) -> FontCharType {
         2 => to_cp437('¶'),
         3 => to_cp437('♣'),
         _ => to_cp437('♥'),
+    }
+}
+
+fn is_revealed_and_wall(map: &Map, x: isize, y: isize) -> bool {
+    map[(x, y)] == TileType::Wall
+}
+
+fn wall_glyph(map: &Map, x: isize, y: isize) -> FontCharType {
+    if x < 1 || x > map.width as isize - 2 || y < 1 || y > map.height as isize - 2 {
+        return 35;
+    }
+    let mut mask: u8 = 0;
+
+    if is_revealed_and_wall(map, x, y - 1) {
+        mask += 1;
+    }
+    if is_revealed_and_wall(map, x, y + 1) {
+        mask += 2;
+    }
+    if is_revealed_and_wall(map, x - 1, y) {
+        mask += 4;
+    }
+    if is_revealed_and_wall(map, x + 1, y) {
+        mask += 8;
+    }
+
+    match mask {
+        0 => 9,    // Pillar because we can't see neighbors
+        1 => 186,  // Wall only to the north
+        2 => 186,  // Wall only to the south
+        3 => 186,  // Wall to the north and south
+        4 => 205,  // Wall only to the west
+        5 => 188,  // Wall to the north and west
+        6 => 187,  // Wall to the south and west
+        7 => 185,  // Wall to the north, south and west
+        8 => 205,  // Wall only to the east
+        9 => 200,  // Wall to the north and east
+        10 => 201, // Wall to the south and east
+        11 => 204, // Wall to the north, south and east
+        12 => 205, // Wall to the east and west
+        13 => 202, // Wall to the east, west, and south
+        14 => 203, // Wall to the east, west, and north
+        15 => 206, // ╬ Wall on all sides
+        _ => 35,   // We missed one?
     }
 }
 
